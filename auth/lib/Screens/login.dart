5531268769c71951c7fcc76d0772af82f45b78ce
mohsinbehzad <mohsinbehzad14@gmail.com>
2025-11-20 00:31:54 +0500

    dispose added for efficiency

--- auth/lib/Screens/login.dart
+++ auth/lib/Screens/login.dart
@@ -20,6 +20,12 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 230, 183, 225),
